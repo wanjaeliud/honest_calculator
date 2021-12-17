54d967e53bdae39f4c482f5a2c8b574ed5a5628d
wanjaeliud <elkwanja@gmail.com>
2021-12-17 21:28:49 +0300

    initial commit

--- Topics/Elif statement/Computer hours/main.py
+++ Topics/Elif statement/Computer hours/main.py
@@ -1,1 +1,8 @@
 # Make sure your output matches the assignment *exactly*
+hour_spent = int(input())
+if hour_spent < 2:
+    print("That's rare nowadays!")
+elif 2 <= hour_spent < 4:
+    print("This seems reasonable")
+else:
+    print("Don't forget to take breaks!")
