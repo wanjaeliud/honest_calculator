03898249c667ab92abcd27eb5ff3a8c448c57d11
Eliud Wanja <45029577+wanjaeliud@users.noreply.github.com>
2022-01-05 22:28:33 +0300

    Update main.py

--- Topics/Elif statement/What day is it?/main.py
+++ Topics/Elif statement/What day is it?/main.py
@@ -1,13 +1,12 @@
 # user_input_time = int(input())
-#
+
 # if user_input_time in [-11, -12]:
 #     print('Monday')
 # elif user_input_time in [14]:
 #     print('Wednesday')
 # elif user_input_time in [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]:
 #     print("Tuesday")
-#
-# But I recommend to use this solution:
+
 
 offset = int(input())
 
